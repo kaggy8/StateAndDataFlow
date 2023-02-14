717d9a3f6c57c0aaeb00f1672cab0b1423a19c3a
Stanislav <kaggy8@ya.ru>
2023-02-14 19:22:15 +0300

    Initial Commit

---
+++ StateAndDataFlow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StateAndDataFlow
+//
+//  Created by Stanislav Demyanov on 14.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Stanislav Demyanov on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
